a8578c59eedf8ff8f4205b504461237f390cc89d
Deepa Kumari <14.11deepa.kumari@gmail.com>
2024-06-06 17:59:21 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,21 +1,4 @@
 set -e
-#!/bin/bash
-
-container_name="my_container"
-
-echo "Stopping container $container_name..."
-docker stop $container_name
-if [ $? -ne 0 ]; then
-  echo "Failed to stop container $container_name"
-  exit 1
-fi
-
-echo "Removing container $container_name..."
-docker rm $container_name
-if [ $? -ne 0 ]; then
-  echo "Failed to remove container $container_name"
-  exit 1
-fi
-
-echo "Container $container_name stopped and removed successfully."
+#!/bin/b
+echo "HI"
 
